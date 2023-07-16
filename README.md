c86ad837d84034b146fd269d8d58c3722d0e1785
Sang Dang <sangdth@gmail.com>
2023-07-16 15:27:22 +0300

    remove booksao, make the template agnostic

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
+Search and replace `nextjs-template` with your project slug.
+
 First, run the development server:
 
 ```bash
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
-export const IS_VERCEL = !!process.env.VERCEL_URL;
+export const IS_VERCEL = !!process.env.VERCEL;
 export const SESSION_NAME = `${IS_VERCEL ? '__Secure-' : ''}next-auth.session-token`;
 
 export const getCookieDomain = () => {
   if (IS_VERCEL && process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
-    return '.booksao.com';
+    return `.${process.env.SITE_DOMAIN}`;
   }
 
   return undefined;
