88b4bfdb2bb2d6bd4c00cc8132febdee6a427801
Sang Dang <sangdth@gmail.com>
2023-08-09 22:44:00 +0300

    improved useAuth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,34 +1,13 @@
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
 import { UserRole } from '@/types';
 
-export type UseAuthOptions = {
-  /**
-   * Specify a path to be redirected to when not login
-   */
-  redirectTo?: string;
-
-  /**
-   * The current page should be protected
-   */
-  required?: boolean;
-};
+export type UseAuthOptions = Parameters<typeof useSession>[0];
 
 export const useAuth = (options?: UseAuthOptions) => {
-  const router = useRouter();
-
-  const { data: session, status } = useSession({
-    required: !!options?.required,
-    onUnauthenticated() {
-      if (!router.pathname.endsWith('/login')) {
-        const redirectTo = router.asPath !== '/login' ? `?redirect=${router.asPath}` : '';
-        router.push(options?.redirectTo ?? `/login${redirectTo}`);
-      }
-    },
-  });
+  const { data: session, status, ...rest } = useSession(options);
 
   const isAdmin = session?.user.role === UserRole.ADMIN;
   const isAuthenticated = !!session && status === 'authenticated';
 
-  return { session, status, isAdmin, isAuthenticated };
+  return { ...rest, session, status, isAdmin, isAuthenticated };
 };
