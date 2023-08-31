4dff350f4795d23c01f58ec6d322251f3394d7cd
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2023-08-31 11:14:22 +0700

    CRUD POST

--- src/pages/api/pages/[pid].ts
+++ src/pages/api/pages/[pid].ts
@@ -1,20 +1,84 @@
 import { prisma } from '@/utils/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const { pid } = req.query;
+  const id = pid?.toString() ?? '';
+  const method = req.method;
 
-  try {
-    const getPost = await prisma.post.findUnique({
-      where: {
-        id: pid?.toString(),
-      },
-    });
-    return res.status(200).json(getPost);
-  } catch (error) {
-    return res.status(400).json(error);
+  switch (method) {
+    case 'GET': {
+      try {
+        const post = await getPostById(id);
+        if (!post) {
+          return NextResponse.json(
+            {
+              messsage: 'Post not found',
+            },
+            { status: 404 }
+          );
+        }
+        return res.status(200).json(post);
+      } catch (error) {
+        return res.status(400).json(error);
+      }
+    }
+    case 'PATCH': {
+      try {
+        const body = await req.body;
+        const { title, content } = body;
+
+        const updatePost = await prisma.post.update({
+          where: {
+            id,
+          },
+          data: {
+            title,
+            content,
+          },
+        });
+        if (!updatePost) {
+          return NextResponse.json(
+            { message: 'Post not found' },
+            { status: 404 }
+          );
+        }
+        return NextResponse.json(updatePost);
+      } catch (error) {
+        return NextResponse.json(
+          { message: 'Update Error', error },
+          { status: 500 }
+        );
+      }
+    }
+    case 'DELETE': {
+      try {
+        await prisma.post.delete({
+          where: {
+            id,
+          },
+        });
+        return NextResponse.json('Post has been deleted');
+      } catch (error) {
+        return NextResponse.json(
+          { message: 'GET Error', error },
+          { status: 500 }
+        );
+      }
+    }
+    default:
+      res.status(400);
+      break;
   }
+}
+async function getPostById(id: string) {
+  return await prisma.post.findUnique({
+    where: {
+      id,
+    },
+  });
 }
--- src/pages/api/pages/posts.ts
+++ src/pages/api/pages/posts.ts
@@ -1,21 +1,40 @@
 import { prisma } from '@/utils/prisma';
 import type { Post } from '@/types';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Post[] | Post>
 ) {
-  if (req.method === 'GET') {
-    const posts = await prisma.post.findMany();
-    res.status(200).json(posts);
-  }
-  if (req.method === 'POST') {
-    const newPost = await prisma.post.create({
-      data: req.body,
-    });
-    res.status(200).json(newPost);
+  const method = req.method;
+
+  switch (method) {
+    case 'GET': {
+      const posts = await getPosts();
+      res.status(200).json(posts);
+      break;
+    }
+    case 'POST': {
+      try {
+        const newPost = await prisma.post.create({
+          data: req.body,
+        });
+        res.status(200).json(newPost);
+      } catch (error) {
+        return NextResponse.json(
+          { message: 'POST error', error },
+          { status: 500 }
+        );
+      }
+      break;
+    }
+    default:
+      res.status(400);
+      break;
   }
 }
 
-// home about-us blog edit-post
+const getPosts = async () => {
+  return await prisma.post.findMany();
+};
