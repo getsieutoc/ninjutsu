8b899b226b0aa45add04210be85e65f0d6731ac4
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2023-09-06 20:20:11 +0700

    Uppercase

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,13 +3,7 @@ import LoginForm from './LoginForm';
 
 export default function Login() {
   return (
-    <Stack
-      gap={4}
-      // alignItems="center"
-      maxWidth="sm"
-      marginX="auto"
-      marginTop="10vh"
-    >
+    <Stack gap={4} maxWidth="sm" marginX="auto" marginTop="10vh">
       <Heading as="h1" size="lg">
         Login
       </Heading>
