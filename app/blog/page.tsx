a97c1432e630ae55622e187544f392b7d43ad400
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2023-09-07 21:48:39 +0700

    create PostCard components

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,18 +1,27 @@
-import { Box, GeneralLayout, Heading, Spinner, Wrap } from '@/components';
+import { prisma } from '@/utils/prisma';
+import {
+  Box,
+  GeneralLayout,
+  Heading,
+  SimpleGrid,
+  Spinner,
+  Wrap,
+} from '@/components';
 import { PostCard } from '@/components/Post';
 
 export default async function BlogsPage() {
   const data = await prisma?.post.findMany({
     orderBy: { createdAt: 'desc' },
+    take: 25,
   });
   if (!data) return <Spinner size="sm" />;
   return (
     <GeneralLayout>
       <Heading>The latest articles</Heading>
       <Box paddingY={5} />
-      <Wrap spacing={5}>
+      <SimpleGrid columns={[2, null, 3]} spacing="10px">
         {data?.map((post, index) => <PostCard key={index} post={post} />)}
-      </Wrap>
+      </SimpleGrid>
     </GeneralLayout>
   );
 }
--- components/Post/PostCard.tsx
+++ components/Post/PostCard.tsx
@@ -1,23 +1,75 @@
 import type { FC, Post } from '@/types';
-import { Box, Card, CardBody, CardFooter, WrapItem } from '@/components';
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Flex,
+  Heading,
+  Spacer,
+  WrapItem,
+} from '@/components';
 import moment from 'moment';
 
 type PropTypes = {
   post: Post;
 };
 export const PostCard: FC<PropTypes> = ({ post }) => {
-  //   console.log(post);
   return (
-    <WrapItem>
+    <Box>
       <Card>
-        <CardBody p={3}>
-          <Box fontSize="sm" color="gray.400">
-            {moment(post.createdAt).format('LL')}
+        <CardBody padding={3} maxHeight={300}>
+          <Flex>
+            <Box fontSize="sm" color="gray.400">
+              {moment(post.createdAt).format('LL')}
+            </Box>
+            <Spacer />
+            <Box>
+              <AvatarGroup size="xs" max={2}>
+                <Avatar
+                  name="Ryan Florence"
+                  src="https://bit.ly/ryan-florence"
+                />
+                <Avatar
+                  name="Segun Adebayo"
+                  src="https://bit.ly/sage-adebayo"
+                />
+                <Avatar name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
+                <Avatar
+                  name="Prosper Otemuyiwa"
+                  src="https://bit.ly/prosper-baba"
+                />
+                <Avatar
+                  name="Christian Nwamba"
+                  src="https://bit.ly/code-beast"
+                />
+              </AvatarGroup>
+            </Box>
+          </Flex>
+          <Box paddingY={3}>
+            <Heading size="md">{post.title}</Heading>
           </Box>
-          <Box></Box>
+          <Box
+            __css={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              display: '-webkit-box',
+              WebkitLineClamp: 5,
+              WebkitBoxOrient: 'vertical',
+            }}
+            as="div"
+            dangerouslySetInnerHTML={{ __html: post?.content ?? '' }}
+          />
         </CardBody>
-        <CardFooter>footer</CardFooter>
+        <CardFooter padding={2}>
+          <Button width="100%" size="sm">
+            Read More
+          </Button>
+        </CardFooter>
       </Card>
-    </WrapItem>
+    </Box>
   );
 };
