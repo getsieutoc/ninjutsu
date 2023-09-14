554dd67721e9083b669eb16868c7688d2eebc947
Sang Dang <sangdth@gmail.com>
2023-09-14 17:23:06 +0300

    remove alter table preferences in previous migration

--- prisma/migrations/20230911085315_add_posttype/migration.sql
+++ prisma/migrations/20230911085315_add_posttype/migration.sql
@@ -4,6 +4,3 @@ CREATE TYPE "PostType" AS ENUM ('DEFAULT', 'AUDIO', 'VIDEO', 'YOUTUBE');
 -- AlterTable
 ALTER TABLE "Post" ADD COLUMN     "featuredImage" TEXT,
 ADD COLUMN     "type" "PostType" DEFAULT 'DEFAULT';
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "preferences" JSONB DEFAULT '{}';
