92e1b6a8813fa23ae31db8a0e9236bd47afb664d
Sang Dang <sangdth@gmail.com>
2023-09-17 13:56:22 +0300

    added some loading

---
+++ app/[locale]/(default)/dashboard/pages/loading.tsx
@@ -0,0 +1,10 @@
+import { Skeleton, SkeletonText, Stack } from '@/components/chakra';
+
+export default function PagesViewLoading() {
+  return (
+    <Stack>
+      <Skeleton height="36px" width="200px" />
+      <SkeletonText mt="4" noOfLines={4} skeletonHeight={4} />
+    </Stack>
+  );
+}
