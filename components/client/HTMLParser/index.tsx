b0039c79f90452bed3ef9669076040c966cbe627
Sang Dang <sangdth@gmail.com>
2023-09-18 08:53:51 +0300

    replace parser to better one

--- components/client/HTMLParser/index.tsx
+++ components/client/HTMLParser/index.tsx
@@ -1,10 +1,19 @@
-import ReactHtmlParser from '@orrisroot/react-html-parser';
+import parser, { type HTMLReactParserOptions } from 'html-react-parser';
 import { Box, BoxProps } from '@/components/chakra';
 
 export type HTMLParserProps = {
   content: string;
+  replace?: HTMLReactParserOptions['replace'];
+  transform?: HTMLReactParserOptions['transform'];
+  trim?: HTMLReactParserOptions['trim'];
 } & BoxProps;
 
-export const HTMLParser = ({ content, ...rest }: HTMLParserProps) => {
-  return <Box {...rest}>{ReactHtmlParser(content)}</Box>;
+export const HTMLParser = ({
+  content,
+  replace,
+  transform,
+  trim,
+  ...rest
+}: HTMLParserProps) => {
+  return <Box {...rest}>{parser(content, { replace, transform, trim })}</Box>;
 };
