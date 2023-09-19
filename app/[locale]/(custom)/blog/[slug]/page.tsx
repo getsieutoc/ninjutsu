6789ef92445a1011b9cee7bf5816cf45c150d64e
Sang Dang <sangdth@gmail.com>
2023-09-19 21:43:27 +0300

    small improvements for TextEditor and single blog post view

--- app/[locale]/(custom)/blog/[slug]/page.tsx
+++ app/[locale]/(custom)/blog/[slug]/page.tsx
@@ -1,13 +1,32 @@
 import { Box, Heading } from '@/components/chakra';
 import { HTMLParser } from '@/components/client';
+import { htmlSanitizer } from '@/utils/parsers';
 import { prisma } from '@/utils/prisma';
+import type { Metadata } from '@/types';
 
-type PagePostProps = {
+type SinglePostViewProps = {
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export default async function PagePost({ params }: PagePostProps) {
+export async function generateMetadata({
+  params,
+}: SinglePostViewProps): Promise<Metadata> {
+  const { slug } = params;
+
+  const post = await prisma.post.findUniqueOrThrow({
+    where: { id: slug },
+    include: { tags: true },
+  });
+
+  return {
+    title: post.title,
+    description: htmlSanitizer(post?.content).slice(0, 200),
+    keywords: post.tags.map(({ value }) => value),
+  };
+}
+
+export default async function SinglePostView({ params }: SinglePostViewProps) {
   const { slug } = params;
 
   const data = await prisma.post.findUnique({ where: { id: slug } });
