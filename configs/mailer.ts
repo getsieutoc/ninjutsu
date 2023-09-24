f1635688d4e714806ff028350ec388cce53c133e
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2023-09-24 21:52:02 +0700

    fix typecheck

--- configs/mailer.ts
+++ configs/mailer.ts
@@ -1,18 +1,19 @@
 import nodemailer from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 const host = process.env.EMAIL_SERVER_HOST;
-const email = process.env.EMAIL_SERVER_USER;
-const pass = process.env.EMAIL_SERVER_PASSWORD;
-const port = process.env.EMAIL_SERVER_PORT;
-
-export const transporter = nodemailer.createTransport({
+const email = process.env.EMAIL_SERVER_USER ?? '';
+const pass = process.env.EMAIL_SERVER_PASSWORD ?? '';
+const port = Number(process.env.EMAIL_SERVER_PORT) ?? 0;
+const transport: SMTPTransport.Options = {
   host,
   port,
   secure: true,
   service: 'gmail',
   auth: {
-    type: 'login',
+    type: 'LOGIN',
     user: email,
     pass,
   },
-});
+};
+export const transporter = nodemailer.createTransport(transport);
