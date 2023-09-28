0254e49908fe9937cd4b30173f8a1371d8f86a46
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2023-09-28 10:03:39 +0700

    create forgot-password form

---
+++ app/[locale]/(auth)/forgot-password/loading.tsx
@@ -0,0 +1,14 @@
+import { Skeleton, Stack } from '@/components/chakra';
+
+export default function LoginLoading() {
+  return (
+    <Stack padding={3} spacing={1} maxW={650} justify="center">
+      <Skeleton height="40px" rounded={5} width="150px" marginBottom={4} />
+
+      <Skeleton height="40px" rounded={5} />
+      <Skeleton height="40px" rounded={5} />
+      <Skeleton height="40px" rounded={5} />
+      <Skeleton height="40px" rounded={5} />
+    </Stack>
+  );
+}
