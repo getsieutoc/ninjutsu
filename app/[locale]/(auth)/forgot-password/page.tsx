c7732ca3399427a8a3f4c02f736ea56fab390ecb
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2023-09-28 10:03:39 +0700

    create forgot-password form

---
+++ app/[locale]/(auth)/forgot-password/page.tsx
@@ -0,0 +1,26 @@
+import { redirect } from 'next/navigation';
+
+import { Text, Stack, Heading, Container } from '@/components/chakra';
+import { getSession } from '@/configs/auth';
+import ForgotPasswordForm from './ForgotPasswordForm';
+
+export default async function Login() {
+  const session = await getSession();
+
+  if (session?.user.id) {
+    redirect('/');
+  }
+  return (
+    <Stack spacing={8} maxWidth="md" marginX="auto" paddingTop="10vh">
+      <Container shadow="base" rounded={10} paddingX={10} padding={10}>
+        <Stack spacing={3} direction="column">
+          <Heading as="h1" size="lg" textAlign="center">
+            Forgot Password
+          </Heading>
+          <Text textAlign="center">Enter Your Email Address</Text>
+          <ForgotPasswordForm />
+        </Stack>
+      </Container>
+    </Stack>
+  );
+}
