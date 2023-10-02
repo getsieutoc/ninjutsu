0021075cd6622da60f4daa4a972ad32637985581
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2023-10-02 20:15:00 +0700

    remove AWS key

--- configs/nodemailer.ts
+++ configs/nodemailer.ts
@@ -2,8 +2,7 @@
 import nodemailer from 'nodemailer';
 // import { defaultProvider } from '@aws-sdk/credential-provider-node';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
-process.env.AWS_ACCESS_KEY_ID = 'AKIA6IJN3CVE2P3WAK2K';
-process.env.AWS_SECRET_ACCESS_KEY = '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';
+
 // const ses = new aws.SES({
 //   apiVersion: "2012-10-17",
 //   region: "us-east-1",
