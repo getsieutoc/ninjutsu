a4a4be68ce3a38e053ac628b8e514babbb574a0a
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2023-10-17 16:07:35 +0700

    update more eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['next/core-web-vitals', 'prettier'],
   rules: {
     'react/display-name': 'off',
-    'no-console': 1, //"off" or 0 - turn the rule off , "warn" or 1 - turn the rule on as a warning (doesn’t affect exit code), "error" or 2 - turn the rule on as an error (exit code will be 1)
+    'no-console': 'error',
   },
 };
